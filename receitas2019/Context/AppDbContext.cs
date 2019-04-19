d47d80198d19d0d516982f38faa66fed3819577e
micael cedro <miicael_@hotmail.com>
2019-04-19 14:32:18 -0300

    commit all

---
+++ receitas2019/Context/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using receitas2019.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace receitas2019.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options)
+        {
+
+        }
+        public DbSet<Receita> Receitas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+    }
+}
